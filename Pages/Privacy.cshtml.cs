b57859bd84b12ddeb3066c1c1c178fd0947dd225
Ganesh Kale <kale.ganesh1511@gmail.com>
2026-02-15 14:29:51 +0530

    Remove DB connection

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -9,28 +9,17 @@ namespace PensionSystem.Pages
     {
         private readonly ILogger<PrivacyModel> _logger;
         private readonly IConfiguration _configuration;
-         private readonly AppDBConnection _context;
 
-
-        public PrivacyModel(ILogger<PrivacyModel> logger,IConfiguration configuration, AppDBConnection appDBConnection)
+        public PrivacyModel(ILogger<PrivacyModel> logger,IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
-            _context = appDBConnection;
         }
 
         public void OnGet()
         {
             ViewData["Value"] = _configuration["DefaultValue"].ToString();
 
-            Person person = new Person
-            {
-                FirstName = "John",
-                LastName = "Doe",
-                DateOfBirth = new DateTime(1990, 1, 1)
-            };
-            _context.Add(person);
-           _context.SaveChanges();
         }
     }
 
